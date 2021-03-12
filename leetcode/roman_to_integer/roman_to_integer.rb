1444df37db5287a4b01dd0baa753b0b07ad167fc
Sean Steel <seanrsteel@gmail.com>
2021-03-12 10:34:29 -0700

    Finish roman to int method

--- leetcode/roman_to_integer/roman_to_integer.rb
+++ leetcode/roman_to_integer/roman_to_integer.rb
@@ -6,6 +6,38 @@ require 'pry'
 
 class RomanInteger
   def roman_to_int(string)
+    total = 0
+    values = map_values(string)
+    index = 0
+    until (index + 1) > string.length
+      begin
+        if values[index] < values[index + 1]
+          total += (values[index + 1] - values[index])
+          index += 2
+        else
+          total += values[index]
+          index += 1
+        end
+      rescue StandardError
+        total += values[index]
+        return total
+      end
+    end
+    total
+  end
 
+  def map_values(string)
+    characters = {
+      'M' => 1000,
+      'D' => 500,
+      'C' => 100,
+      'L' => 50,
+      'X' => 10,
+      'V' => 5,
+      'I' => 1
+    }
+    string.chars.map do |char|
+      characters[char]
+    end
   end
 end
