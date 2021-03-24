def7a135cca8072f80f4263c411efc16b2aced65
Sean Steel <seanrsteel@gmail.com>
2021-03-24 11:18:02 -0600

    Fix mistake in the is prime method for the prime number algorithm challenge

--- ruby_coding_challenge/prime_number_algorithm.rb
+++ ruby_coding_challenge/prime_number_algorithm.rb
@@ -16,7 +16,7 @@ end
 
 # def is_prime(item)
 #   return false if item == 1
-#   (2..(item-1)).each do |item|
+#   (2..(item-1)).each do |number|
 #     if item % number == 0
 #       return false
 #     end
