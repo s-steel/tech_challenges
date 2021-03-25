20085194754ecdfffb34bf06522596eb890043a5
Sean Steel <seanrsteel@gmail.com>
2021-03-25 10:37:15 -0600

    Add another way to complete max product subarray challenge

--- leetcode/max_product_subarray/max_product_subarray.rb
+++ leetcode/max_product_subarray/max_product_subarray.rb
@@ -1,16 +1,24 @@
 require 'pry'
 
 class MaxProductSubarray
+  # def max_product(nums)
+  #   reverse_nums = nums.reverse
+  #   (1..nums.length-1).each do |x|
+  #     if nums[x-1] != 0
+  #       nums[x] *= nums[x-1]
+  #     end
+  #     if reverse_nums[x-1] != 0
+  #       reverse_nums[x] *= reverse_nums[x-1]
+  #     end
+  #   end
+  #   (nums + reverse_nums).max
+  # end
+
   def max_product(nums)
-    reverse_nums = nums.reverse
-    (1..nums.length-1).each do |x|
-      if nums[x-1] != 0
-        nums[x] *= nums[x-1]
+    (0..nums.length).map do |i|
+      (i..nums.length).map do |j|
+        nums[i..j].reduce(:*)
       end
-      if reverse_nums[x-1] != 0
-        reverse_nums[x] *= reverse_nums[x-1]
-      end
-    end
-    (nums + reverse_nums).max
+    end.flatten.reject(&:nil?).max
   end
 end
