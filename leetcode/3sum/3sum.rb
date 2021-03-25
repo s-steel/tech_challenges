3946ee311f1eb2378e3b25760de2e9775fee28e1
Sean Steel <seanrsteel@gmail.com>
2021-03-25 12:05:00 -0600

    Refactor leetcode three sum challenge

--- leetcode/3sum/3sum.rb
+++ leetcode/3sum/3sum.rb
@@ -4,9 +4,8 @@ class ThreeSum
   def three_sum(nums)
     output = []
     for index in 0..nums.length do
-      output = output + nums.combination(3).to_a
+      output = output + nums.combination(3).select { |a,b,c| (a+b+c) == 0 }.map { |i| i.sort }
     end
-    all_sums = output.select { |i| i.sum == 0 && i.length == 3 }
-    all_sums.map { |i| i.sort }.uniq.sort
+    output.uniq.sort
   end
 end
