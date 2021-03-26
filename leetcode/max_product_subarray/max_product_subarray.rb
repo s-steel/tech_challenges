7d730202adf33a0dcc4ba1a5e2c4043bc0858bf9
Sean Steel <seanrsteel@gmail.com>
2021-03-26 11:57:02 -0600

    Discuss differences in two solutions for max product subarray with group

--- leetcode/max_product_subarray/max_product_subarray.rb
+++ leetcode/max_product_subarray/max_product_subarray.rb
@@ -1,24 +1,26 @@
 require 'pry'
 
 class MaxProductSubarray
-  # def max_product(nums)
-  #   reverse_nums = nums.reverse
-  #   (1..nums.length-1).each do |x|
-  #     if nums[x-1] != 0
-  #       nums[x] *= nums[x-1]
-  #     end
-  #     if reverse_nums[x-1] != 0
-  #       reverse_nums[x] *= reverse_nums[x-1]
-  #     end
-  #   end
-  #   (nums + reverse_nums).max
-  # end
-
   def max_product(nums)
-    (0..nums.length).map do |i|
-      (i..nums.length).map do |j|
-        nums[i..j].reduce(:*)
+    reverse_nums = nums.reverse
+    (1..nums.length-1).each do |x|
+      if nums[x-1] != 0
+        nums[x] *= nums[x-1]
       end
-    end.flatten.reject(&:nil?).max
+      if reverse_nums[x-1] != 0
+        reverse_nums[x] *= reverse_nums[x-1]
+      end
+    end
+    (nums + reverse_nums).max
   end
+
+
+# Time limit exceeded for this one:
+  # def max_product(nums)
+  #   (0..nums.length).map do |i|
+  #     (i..nums.length).map do |j|
+  #       nums[i..j].reduce(:*)
+  #     end
+  #   end.flatten.reject(&:nil?).max
+  # end
 end
